e4a970c21c3dc7ece1c686c28add499697f262ad
Rahul Kumar <121325179+Rahulkumar132@users.noreply.github.com>
2023-07-15 15:59:25 +0530

    Create E-Commerce.js

---
+++ E-Commerce.js
@@ -0,0 +1,10 @@
+# -Rahul kumar-INBT0817-jul/2023
+  const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
